284f70f84dc468ff649eaa334b19d6e0de08cb93
Manuel Antonio Samaniego Serrano <95022226+ManuelsSaNt@users.noreply.github.com>
2024-02-02 12:30:37 -0700

    init desktop app init window, db made it, slq in magnament studio

--- CapaPresentacion/Inicio.Designer.cs
+++ CapaPresentacion/Inicio.Designer.cs
@@ -29,11 +29,64 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.Lavender;
+			this.menuStrip1.Location = new System.Drawing.Point(0, 70);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menu";
+			// 
+			// menuStrip2
+			// 
+			this.menuStrip2.AutoSize = false;
+			this.menuStrip2.BackColor = System.Drawing.Color.MidnightBlue;
+			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip2.Name = "menuStrip2";
+			this.menuStrip2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.menuStrip2.Size = new System.Drawing.Size(884, 70);
+			this.menuStrip2.TabIndex = 1;
+			this.menuStrip2.Text = "menuTitulo";
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(304, 39);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Sistema de Ventas";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// Inicio
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(884, 465);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.menuStrip2);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Inicio";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+
+		private System.Windows.Forms.Label label1;
+
+		private System.Windows.Forms.MenuStrip menuStrip2;
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
 
 		#endregion
 
--- CapaPresentacion/Inicio.cs
+++ CapaPresentacion/Inicio.cs
@@ -16,5 +16,10 @@ namespace SistemaDeVentas
 		{
 			InitializeComponent();
 		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
